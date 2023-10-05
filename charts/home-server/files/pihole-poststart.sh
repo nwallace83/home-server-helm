033d5a9a5388cc0174a2833a222affbd9847a998
Nathan Wallace <nathan.wallace114@gmail.com>
2023-10-05 02:19:41 -0600

    fix more bugs

--- charts/home-server/files/pihole-poststart.sh
+++ charts/home-server/files/pihole-poststart.sh
@@ -4,8 +4,7 @@ COUNT=1
 GRAVITY_FILE="/etc/pihole/gravity.db"
 while [ ! -f $GRAVITY_FILE ]
 do
-  echo "Attempt #$COUNT"
-  echo "Waiting for /etc/pihole/gravity.db to exist"
+  echo "Waiting for /etc/pihole/gravity.db to exist #$COUNT"
   COUNT=$(expr $COUNT + 1)
   sleep 1
 done
@@ -28,7 +27,7 @@ echo "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.
 
 RESULT=1
 COUNT=1
-while [ $RESULT != 0 ]
+while [ $RESULT -ne 0 ]
 do
   echo "Attempt #$COUNT"
   RESULT=$(sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://dbl.oisd.nl',1,'');")
@@ -38,5 +37,8 @@ done
 
 echo "https://dbl.oisd.nl done"
 
+sleep 60
 echo "Updating gravity"
 pihole updateGravity
+
+echo "poststart script complete"
