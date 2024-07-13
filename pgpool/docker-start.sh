eff4e4a0966c797f3fb99411a10bbafad5441f99
Nathan Wallace <nathan.wallace114@gmail.com>
2024-07-13 00:23:59 -0600

    add features

--- pgpool/docker-start.sh
+++ pgpool/docker-start.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-if [ -z $DBUSER ] || [ -z $DBPASSWORD ]; then
-  echo "Missing one or more variables: DBUSER, DBPASSWORD"
+if [ ! -e /app/database-credentials/username ] || [ ! -e /app/database-credentials/password ]; then
+  echo "Missing one or more files:"
+  echo "- /app/database-credentials/username"
+  echo "- /app/database-credentials/password"
   exit 1
 fi
 
-echo "$DBUSER:$DBPASSWORD" >> /tmp/pool_passwd
+if [ -z $PGHOSTADDR ] || [ -z $PGPORT ] || [ -z $PGDATABASE ]; then
+  echo "Missing one or more variables: PGHOSTADDR PGPORT PGDATABASE"
+  exit 1
+fi
+
+DBUSERNAME=$(cat /app/database-credentials/username)
+DBPASSWORD=$(cat /app/database-credentials/password)
+
+echo "$DBUSERNAME:$DBPASSWORD" >> /tmp/pool_passwd
+echo "$PGHOSTADDR:$PGPORT:$PGDATABASE:$DBUSERNAME:$DBPASSWORD" > /app/.pgpass
 
 pgpool -n
